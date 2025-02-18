924e6865fa4c8d8c5c67fc7a8cc1cb66dadd3709
Chaitanya Pawar <chaitanya09pawar@gmail.com>
2025-02-18 13:08:53 +0530

    monkey

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://chaitanya09pawar:admin@kalvi.6sq9n.mongodb.net/");
+        await mongoose.connect("monkey");
         console.log("MongoDB is connected");
     } catch (error) {
         console.error("MongoDB connection failed:", error.message);
